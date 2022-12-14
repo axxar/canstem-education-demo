9f72fbdb775ffa7fb81c73d590b165e95f890292
Axxar <67249459+axxar@users.noreply.github.com>
2022-12-14 18:13:54 -0500

    padding issue fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div class="">
       <div class="relative container mx-auto">
         <img src={background} alt="Classroom with students" />
-        <div class="absolute w-full lg:mx-0 font-semibold text-center text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-sans p-10 text-xl lg:text-5xl ">
+        <div class="absolute w-full lg:mx-0 font-semibold text-center text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-sans text-2xl lg:text-5xl ">
           <p className="leading-normal lg:leading-relaxed">
             Day School,
           </p>
