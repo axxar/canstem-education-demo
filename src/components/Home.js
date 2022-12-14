6f53a02bd36548d7f35af0a38429336b807fb291
Axxar <67249459+axxar@users.noreply.github.com>
2022-12-14 17:03:38 -0500

    NavBar mobile fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
   return (
     <div class="relative container mx-auto">
       <img src={background} alt="Classroom with students" />
-      <div class="absolute font-semibold text-center text-white top-1/4 -translate-y-1/4 font-sans p-10">
-        <p className="text-5xl leading-normal">Day School,</p>
-        <p className="text-5xl leading-normal">High School Credit Courses,</p>
-        <p className="text-5xl leading-normal">Tutoring And After School Help</p>
+      <div class="absolute font-semibold text-center text-white mx-auto top-1/2 -translate-y-1/2 lg:top-1/4 lg:-translate-y-1/4 font-sans p-10">
+        <p className="text-md lg:text-5xl leading-normal lg:leading-relaxed">Day School,</p>
+        <p className="text-md lg:text-5xl leading-normal lg:leading-relaxed">High School Credit Courses,</p>
+        <p className="text-md lg:text-5xl leading-normal lg:leading-relaxed">Tutoring And After School Help</p>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,123 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Transition } from "@headlessui/react";
 import CSELogo from "../components/images/CSE-logo.png";
 import "flowbite";
 
 function Navbar({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-        <div class="container flex flex-wrap items-center justify-between mx-auto">
-          <NavLink to="/" class="">
-            <img
-              src={CSELogo}
-              class="inline h-16"
-              alt="CanSTEM Education Logo"
-            />
-            <span class="self-center align-middle text-3xl font-semibold whitespace-nowrap text-green-600 hover:text-black">
-              CanSTEM Education
-            </span>
-          </NavLink>
-          <button
-            data-collapse-toggle="navbar-dropdown"
-            type="button"
-            class="inline-flex items-center p-2 ml-3 text-lg text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-dropdown"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-            <ul class="flex flex-col p-4 mt-4  md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium md:border-0 md:bg-white">
-              <li>
+      <nav className="bg-white sticky top-0 z-10  border-b border-gray-200 text-green-600">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 ">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center text-lg tracking-wider">
+                <NavLink to="/" className="flex items-center hover:text-blue-700">
+                  <img className="h-10 lg:h-15 " src={CSELogo} alt="CSE logo white" />
+                  <span class="self-center text-md md:text-2xl align-middle font-semibold whitespace-nowrap">CanSTEM Education</span>
+                </NavLink>
+            </div>
+
+            <div className="-mr-2 flex">
+            <div className="hidden md:block">
+                <div className="ml-10 flex space-x-4 font-medium text-md">
+                  <NavLink
+                    to="/"
+                    className="px-3 py-2 hover:text-blue-700"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to="/Services"
+                    className="px-3 py-2 hover:text-blue-700"
+                  >
+                    Services
+                  </NavLink>
+                  <NavLink
+                    to="/ContactUs"
+                    className="px-3 py-2 hover:text-blue-700"
+                  >
+                    Contact Us
+                  </NavLink>
+                </div>
+              </div>
+              <div className="md:hidden ml-2">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {!isOpen ? (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden font-AdventPro tracking-wide" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <NavLink
                   to="/"
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                  aria-current="page"
+                  className="hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Home
                 </NavLink>
-              </li>
-              <li>
-                <button
-                  id="dropdownNavbarLink"
-                  data-dropdown-toggle="dropdownNavbar"
-                  class="flex items-center justify-start w-full py-2 pr-4 font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto"
-                >
-                  Services{" "}
-                  <svg
-                    class="w-5 h-5 ml-1"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
-                <div
-                  id="dropdownNavbar"
-                  class="z-10 hidden font-normal bg-white divide-gray-100 rounded shadow"
-                >
-                  <ul
-                    class="py-1 text-gray-700 dark:text-gray-400"
-                    aria-labelledby="dropdownLargeButton"
-                  >
-                    <li>
-                      <NavLink to="/DaySchool">
-                        <span class="block px-4 py-2 hover:bg-gray-100">
-                          Day School
-                        </span>
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink to="/HighSchoolCreditCourses">
-                        <span class="block px-4 py-2 hover:bg-gray-100">
-                          High School Credit Courses
-                        </span>
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink to="/Tutoring">
-                        <span class="block px-4 py-2 hover:bg-gray-100">
-                          Tutoring
-                        </span>
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li>
+
                 <NavLink
-                  to="/contact"
-                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                  to="/Services"
+                  className="hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Contact
+                  Services
                 </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
+
+                <NavLink
+                  to="/ContactUs"
+                  className="hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Contact Us
+                </NavLink>
+              </div>
+            </div>
+          )}
+        </Transition>
       </nav>
       <main>
-        <div className="mt-14 mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+        <div className="container mt-2 mx-auto sm:px-6 lg:px-8">{children}</div>
       </main>
     </div>
   );
