e686118bc2de4bfccfc1e1982f6929539c46ba2c
Axxar <67249459+axxar@users.noreply.github.com>
2022-12-14 18:07:10 -0500

    home text fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,14 @@ const Home = () => {
     <div class="">
       <div class="relative container mx-auto">
         <img src={background} alt="Classroom with students" />
-        <div class="absolute w-full lg:mx-0 font-semibold text-center text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-sans p-10">
-          <p className="text-lg lg:text-5xl leading-normal lg:leading-relaxed">
+        <div class="absolute w-full lg:mx-0 font-semibold text-center text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-sans p-10 text-xl lg:text-5xl ">
+          <p className="leading-normal lg:leading-relaxed">
             Day School,
           </p>
-          <p className="text-lg lg:text-5xl leading-normal lg:leading-relaxed">
+          <p className="leading-normal lg:leading-relaxed">
             High School Credit Courses,
           </p>
-          <p className="text-lg lg:text-5xl leading-normal lg:leading-relaxed">
+          <p className="leading-normal lg:leading-relaxed">
             Tutoring And After School Help
           </p>
         </div>
