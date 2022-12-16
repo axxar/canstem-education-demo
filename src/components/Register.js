ed9567e948814302ffa068c3292e8ec3ccddaf48
Axxar <67249459+axxar@users.noreply.github.com>
2022-12-15 21:19:43 -0500

    netlify form

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,9 +8,10 @@ const Register = () => {
           <div class="md:shrink-0 sm:p-0 px-2 md:w-6/12">
             <h1 className="text-4xl leading-normal">Register</h1>
             <form
-              name="contact"
+              name="register"
               method="POST"
               data-netlify-recaptcha="true"
+              netlify-honeypot="bot-field"
               data-netlify="true"
               className=""
             >
@@ -21,34 +22,38 @@ const Register = () => {
                 >
                   Your email
                 </label>
-                <input
-                  type="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                  placeholder="name@flowbite.com"
-                  required
-                />
+                <p>
+                  <input
+                    type="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="name@flowbite.com"
+                    required
+                  />
+                </p>
               </div>
               <div class="mb-6">
                 <label
                   for="password"
                   class="block mb-2 text-sm font-medium text-gray-900"
                 >
-                  Your password
+                  Your message
                 </label>
-                <input
-                  type="password"
-                  id="password"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                  required
-                />
+                <p>
+                  <textarea
+                    id="message"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    required
+                  ></textarea>
+                </p>
               </div>
-              <button
-                type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-              >
-                Submit
-              </button>
+              <p>
+                <input
+                  type="submit"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                  value="Submit"
+                />
+              </p>
             </form>
           </div>
         </div>
