301fe5072ba1e4705c1434d382cc935efe7f3bdc
Axxar <67249459+axxar@users.noreply.github.com>
2022-12-15 21:28:37 -0500

    test demo form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,64 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { NetlifyForm, Honeypot } from "react-netlify-forms";
 
 const Register = () => {
   return (
-    <div className="flex mx-auto">
-      <div className="container">
-        <div class="md:flex">
-          <div class="md:shrink-0 sm:p-0 px-2 md:w-6/12">
-            <h1 className="text-4xl leading-normal">Register</h1>
-            <form
-              name="register"
-              method="POST"
-              data-netlify-recaptcha="true"
-              netlify-honeypot="bot-field"
-              data-netlify="true"
-              className=""
-            >
-              <div class="mb-6">
-                <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <p>
-                  <input
-                    type="email"
-                    id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                    placeholder="name@flowbite.com"
-                    required
-                  />
-                </p>
-              </div>
-              <div class="mb-6">
-                <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your message
-                </label>
-                <p>
-                  <textarea
-                    id="message"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                    required
-                  ></textarea>
-                </p>
-              </div>
-              <p>
-                <input
-                  type="submit"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-                  value="Submit"
-                />
-              </p>
-            </form>
+    <NetlifyForm name="Contact" action="/thanks" honeypotName="bot-field">
+      {({ handleChange, success, error }) => (
+        <>
+          <Honeypot />
+          {success && <p>Thanks for contacting us!</p>}
+          {error && (
+            <p>
+              Sorry, we could not reach our servers. Please try again later.
+            </p>
+          )}
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" onChange={handleChange} />
           </div>
-        </div>
-      </div>
-    </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              type="text"
+              name="message"
+              id="message"
+              rows="4"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </>
+      )}
+    </NetlifyForm>
   );
 };
 
