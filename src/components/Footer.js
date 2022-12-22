97a8a07db1ab2d1e61c97c5ca92132c3882e2301
Axxar <67249459+axxar@users.noreply.github.com>
2022-12-22 18:31:20 -0500

    tel for anchor tag for clickable phone number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,29 +16,37 @@ const Footer = () => {
             </NavLink>
             <ul class="text-gray-600 dark:text-gray-400 text-sm lg:text-md font-semibold mt-2">
               <li class="mb-3">
-                <svg
-                  className="w-5 h-5 font-bold inline-block mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                </svg>
-                647-709-0258
+                <a href="tel:647-709-0258" target="_blank" rel="noreferrer">
+                  <svg
+                    className="w-5 h-5 font-bold inline-block mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  </svg>
+                  647-709-0258
+                </a>
               </li>
               <li class="mb-3">
-                <svg
-                  className="w-5 h-5 font-bold inline-block mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                </svg>
-                647-568-0258
+                <a href="tel:647-568-0258" target="_blank" rel="noreferrer">
+                  <svg
+                    className="w-5 h-5 font-bold inline-block mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  </svg>
+                  647-568-0258
+                </a>
               </li>
               <li>
-                <a href="mailto:canstem.education@gmail.com" target="_blank" rel="noreferrer">
+                <a
+                  href="mailto:canstem.education@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <svg
                     class="w-5 h-5 font-bold inline-block mr-2"
                     fill="currentColor"
